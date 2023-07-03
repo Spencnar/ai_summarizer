830bf01f14b14bd501908aa7940b13d4e9431834
Spencer Hawhee <teamfallenhax@gmail.com>
2023-07-03 13:38:03 -0400

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import { logo } from '../assets';
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
+        <button type='button' onClick={() => window.open('https://github.com/Spencnar')} className='black_btn'>GitHub</button>
+      </nav>
+
+      <h1 className='head_text'>Summarize Article with <br className='max-md:hidden' />
+      <span className='blue_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify online articles with ease with our AI Summarizer, one copy and paste makes articles into clear summaries.
+      </h2>
+    </header>
+  )
+}
+
+export default Hero
